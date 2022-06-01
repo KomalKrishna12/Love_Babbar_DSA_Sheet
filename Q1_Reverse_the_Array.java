63e990d7c7687ee674c4beeb68189876faab8339
KomalKrishna12 <komalkrishna016@gmail.com>
2022-06-01 22:47:26 +0530

    reverse the array

--- Q1_Reverse_the_Array.java
+++ Q1_Reverse_the_Array.java
@@ -4,20 +4,33 @@ public class Q1_Reverse_the_Array{
         arr[li] = arr[ri];
         arr[ri] = temp;
     }
+
+    public static void reverseArray(int[] arr, int li, int ri){
+        if(li >= ri) return;
+        swap(li, ri, arr);
+        reverseArray(arr, li+1, ri-1);
+    }
     public static void main(String[] args) {
         int arr[] = {4, 5, 1, 2};
 
-        for(int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
+        // iterative
+        // for(int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
 
-        int li = 0, ri = arr.length - 1;
+        // int li = 0, ri = arr.length - 1;
 
-        while(li < ri){
-            swap(li, ri, arr);
-            li++;
-            ri--;
-        }
+        // while(li < ri){
+        //     swap(li, ri, arr);
+        //     li++;
+        //     ri--;
+        // }
 
-        System.out.println();
+        // System.out.println();
+
+        // for(int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
+
+
+        // recursion
+        reverseArray(arr, 0, arr.length - 1);
 
         for(int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
 
