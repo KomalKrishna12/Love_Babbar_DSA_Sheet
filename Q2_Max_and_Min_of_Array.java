76252404074095611e2c44d98a145f4879340418
KomalKrishna12 <komalkrishna016@gmail.com>
2022-06-02 11:02:46 +0530

    max min of array

--- Q2_Max_and_Min_of_Array.java
+++ Q2_Max_and_Min_of_Array.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 public class Q2_Max_and_Min_of_Array {
     public static void main(String[] args) {
+        int[] arr = {1, 0, 4, 6, 2};
         
+        Arrays.sort(arr);
+
+        System.out.println("max : " + arr[arr.length - 1] + ", min : " + arr[0]);
     }
 }
