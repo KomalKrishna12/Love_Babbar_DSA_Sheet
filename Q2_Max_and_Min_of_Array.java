d6a9e18ec2bd72e41162906566548cd59f87809e
KomalKrishna12 <komalkrishna016@gmail.com>
2022-06-02 14:16:43 +0530

    max and min of array

--- Q2_Max_and_Min_of_Array.java
+++ Q2_Max_and_Min_of_Array.java
@@ -4,8 +4,19 @@ public class Q2_Max_and_Min_of_Array {
     public static void main(String[] args) {
         int[] arr = {1, 0, 4, 6, 2};
         
-        Arrays.sort(arr);
+        // method 1
+        // sort the array
+        // after sorting first element of array will be min and last will be max if array is sorted
+        // in ascending order
+        // Arrays.sort(arr);
+        // System.out.println("max : " + arr[arr.length - 1] + ", min : " + arr[0]);
 
-        System.out.println("max : " + arr[arr.length - 1] + ", min : " + arr[0]);
+        int max = arr[0];
+        int min = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            max = Math.max(max, arr[i]);
+            min = Math.min(min, arr[i]);
+        }
+        System.out.println("max : " + max + ", min : " + min);
     }
 }
